1f599d79ca08b6465f292d63094e8fa9abb18da5
Samuel-045 <samuelvic856@gmail.com>
2023-01-03 12:47:20 -0300

    Mudança no projeto, junto com a inserção de um formulário para cadastro de email e nome e a criação de header e footer.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-//let nome= prompt("Qual carro desejado")
+window.onload = function (){
+
+    var bttn = document.getElementById("submit")
+    var mensagem = document.getElementById("finalizacao")
+    var dpsgmail = document.getElementById("gmail")
+    var header = document.getElementById("header")
+    var navegation_header = document.getElementById("navigation_header")
+    var showsidebar= "false"
+
+    document.addEventListener("visibilitychange", (event)=> {
+        if (document.visibilityState==="visible"){
+            document.title= "De Bugatti a Rolls-Royce: conheça os 10 carros mais caros do mundo";
+        }else{
+            document.title= "Hey, continue lendo"
+        }
+    })
+
+
+   
+
+}
