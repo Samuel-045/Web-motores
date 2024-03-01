ce7c6d69114c2848ede8ae1483136fc97f66cf93
Samuel-045 <samuelvic856@gmail.com>
2024-03-01 19:16:28 -0300

    Criação de uma nova página e modificação no footer

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
 window.onload = function (){
-    //função para alterar o titulo da página
-    document.addEventListener("visibilitychange", (event)=> {
-        if (document.visibilityState==="visible"){
-            document.title= "De Bugatti a Rolls-Royce: conheça os 10 carros mais caros do mundo";
-        }else{
-            document.title= "Hey, continue lendo"
-        }
-    })
-
-
-    //const respGet = () => JSON.parse(localStorage.getItem('registro')) ?? []
-    //const respSet = (obj) => localStorage.setItem('registro',JSON.stringify(obj));
-
+   
     const respGet = () => JSON.parse(localStorage.getItem('WM-registro')) ?? []
     const respSet = (obj) => localStorage.setItem('WM-registro',JSON.stringify(obj))
     function verificar(email){
